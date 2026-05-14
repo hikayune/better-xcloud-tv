e2c6cdba8581873c33fc4cb333ff3702e69a657b
hikayume404 <131531729+hikayune@users.noreply.github.com>
2026-05-14 16:44:56 -0300

    Use manual xCloud launcher

--- webos/src/index.js
+++ webos/src/index.js
@@ -1,1 +1,8 @@
-window.location = 'https://www.xbox.com/en-US/play/login/deviceCode';
+function openXcloud() {
+  location.href = 'https://www.xbox.com/play';
+}
+
+window.addEventListener('load', function () {
+  var button = document.getElementById('open');
+  if (button) button.addEventListener('click', openXcloud);
+});
